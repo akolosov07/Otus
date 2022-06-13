0e4fe6297281fd940bcb0e2fea81b321af3dbc4f
Alexander <avt757@hotmail.com>
2022-06-13 15:49:40 +0300

    charge service

---
+++ ChargeService/ChargeService/ChargeService.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using ChargeService.Utility.Exceptions;
+using ChargeService.Utility.Models;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ChargeService.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, IWebHostEnvironment env, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _env = env;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsynce(context, ex, _env);
+            }
+        }
+
+        private async Task HandleExceptionAsynce(HttpContext context, Exception exception, IWebHostEnvironment env)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var errors = new ApiErrorResponse()
+            {
+                StatusCode = (int)code
+            };
+
+            if (_env.IsDevelopment())
+            {
+                errors.Details = exception.Message;
+                errors.StackTrace = exception?.StackTrace;
+            }
+
+            _logger.LogError(exception, $"Error handling");
+
+            switch (exception)
+            {
+                case ApplicationValidationException e:
+                    errors.Message = e.Message;
+                    errors.StatusCode = (int)HttpStatusCode.UnprocessableEntity;
+                    break;
+                default:
+                    errors.Message = "Something is wrong in our system";
+                    errors.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            var result = JsonConvert.SerializeObject(errors);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = errors.StatusCode;
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ ChargeService/ChargeService/ChargeService.MessageBroker.Consumer/Services/UpdateRequestMQStatusConsumer.cs
@@ -0,0 +1,97 @@
+﻿using ChargeService.BLL.Services.Interfaces;
+using ChargeService.MessageBroker.Entities;
+using ChargeService.MessageBroker.Settings;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ChargeService.MessageBroker.Consumer.Services
+{
+    public class UpdateRequestMQStatusConsumer : BackgroundService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+        private readonly RabbitMQSettings _rabbitMQSettings;
+        private readonly ILogger<UpdateRequestMQStatusConsumer> _logger;
+        private readonly IServiceProvider _serviceProvider;
+
+        public UpdateRequestMQStatusConsumer(IOptions<RabbitMQSettings> rabbitMQSettings,
+            IServiceProvider serviceProvider, ILogger<UpdateRequestMQStatusConsumer> logger)
+        {
+            _logger = logger;
+
+            try
+            {
+                _serviceProvider = serviceProvider;
+                _rabbitMQSettings = rabbitMQSettings.Value;
+                var factory = new ConnectionFactory
+                {
+                    HostName = _rabbitMQSettings.Hostname,
+                    UserName = _rabbitMQSettings.UserName,
+                    Password = _rabbitMQSettings.Password,
+                    VirtualHost = _rabbitMQSettings.VirtualHost
+                };
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+                _channel.QueueDeclare(
+                    queue: _rabbitMQSettings.UpdateRequestMQ,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"UpdateRequestMQStatusConsumer ctor Error");
+            }
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += async (ch, ea) =>
+            {
+                var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+
+                try
+                {
+                    var updateRequest = JsonConvert.DeserializeObject<UpdateRequestMQStatus>(content);
+
+                    using (IServiceScope scope = _serviceProvider.CreateScope())
+                    {
+                        ISessionService sessionService = scope.ServiceProvider.GetRequiredService<ISessionService>();
+                        await sessionService.UpdateStatusAsync(updateRequest.RequestId, updateRequest.Status);
+                    }
+
+                    _channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"UpdateRequestMQStatusConsumer Received Error");
+                } 
+            };
+            try
+            {
+                _channel.BasicConsume(_rabbitMQSettings.UpdateRequestMQ, false, consumer);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, $"UpdateRequestMQStatusConsumer BasicConsume Error");
+            }
+            
+
+            return Task.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            _channel.Close();
+            _connection.Close();
+            base.Dispose();
+        }
+    }
+}
